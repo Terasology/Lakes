271f3b4ce311bf983b72ab99a43271b0de0ff869
Cervator <cervator@gmail.com>
2020-07-06 00:25:35 -0500

    chore: Adjust for engine changes in https://github.com/MovingBlocks/Terasology/pull/4029

--- src/main/java/org/terasology/Lakes/LakeRasterizer.java
+++ src/main/java/org/terasology/Lakes/LakeRasterizer.java
@@ -66,15 +66,15 @@ public class LakeRasterizer implements WorldRasterizerPlugin {
 
                 if (lake.LakeContains(position) && position.y() <= lake.getWaterHeight() && (position.y() >= lake.getWaterHeight() - lakeDepth ||
                     position.y() > surfaceHeight)) {
-                    chunk.setBlock(ChunkMath.calcBlockPos(position), water);
+                    chunk.setBlock(ChunkMath.calcRelativeBlockPos(position), water);
                 }
 
                else if (lake.OuterContains(position) && position.y() <= lake.getWaterHeight() && position.y() >= surfaceHeight) {
-                    chunk.setBlock(ChunkMath.calcBlockPos(position), sand);
+                    chunk.setBlock(ChunkMath.calcRelativeBlockPos(position), sand);
                 }
 
                 else if (lake.LakeContains(position) && position.y() > lake.getWaterHeight() && position.y() <= lake.getWaterHeight() + lakeHeight) {
-                    chunk.setBlock(ChunkMath.calcBlockPos(position), air);
+                    chunk.setBlock(ChunkMath.calcRelativeBlockPos(position), air);
                 }
             }
 
@@ -86,15 +86,15 @@ public class LakeRasterizer implements WorldRasterizerPlugin {
 
                 if (lavaLake.LakeContains(position) && position.y() <= lavaLake.getWaterHeight() && (position.y() >= lavaLake.getWaterHeight() - lakeDepth ||
                         position.y() > surfaceHeight)) {
-                    chunk.setBlock(ChunkMath.calcBlockPos(position), lava);
+                    chunk.setBlock(ChunkMath.calcRelativeBlockPos(position), lava);
                 }
 
                 else if (lavaLake.OuterContains(position) && position.y() <= lavaLake.getWaterHeight() && position.y() >= surfaceHeight) {
-                    chunk.setBlock(ChunkMath.calcBlockPos(position), stone);
+                    chunk.setBlock(ChunkMath.calcRelativeBlockPos(position), stone);
                 }
 
                 else if (lavaLake.LakeContains(position) && position.y() > lavaLake.getWaterHeight() && position.y() <= lavaLake.getWaterHeight() + lakeHeight) {
-                    chunk.setBlock(ChunkMath.calcBlockPos(position), air);
+                    chunk.setBlock(ChunkMath.calcRelativeBlockPos(position), air);
                 }
             }
 
